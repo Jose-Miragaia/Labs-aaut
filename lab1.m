b6392eb1807ee5f2415ccd2f727de989ac8f279b
Jose-Miragaia <jpedroboavida@hotmail.com>
2017-10-03 15:22:35 +0100

    hi
    hi

---
+++ lab1.m
@@ -0,0 +1,10 @@
+%%ramiro e Miragaia
+
+%%
+%Pergunta 1;
+clear 
+close all
+load data1
+%3.a)è pedido que a ordem 1 logo p =1
+matrizfazer(1,x,y)
+%3.b)
---
+++ matrizfazer.m
@@ -0,0 +1,46 @@
+function  [a,beta] = matrizfazer(p,X,Y)
+      close all
+      n = size (X,1);
+     % p = size (X,2);
+
+      a = ones (n,p+1);
+      
+      for (i=2:p+1)
+        a(:,i) = X(:,1).^(i-1);
+      end
+      
+       beta = a\Y;
+       figure
+       hold on
+       
+       scatter(X,Y)
+       t= min(X):1E-3:max(X);
+%        a = size(t);
+%        for j=1:a(2)
+%             FUNC(j) = 0;
+%            end
+%        
+%        for i=1:p+1    
+%            for j=1:a(2)
+%             FUNC(j) = FUNC(j) + beta(i)*t(j)^(i-1);
+%            end
+%        end
+       
+       plot (t,funcaomagica(beta,t))
+    
+       erro =0;
+       
+        for i=1:n
+             erro = erro + ((Y(i) - funcaomagica(beta,X(i)))^2);
+        end
+        erro
+       
+end
+
+
+ function output = funcaomagica(beta,x)
+    output=0;
+    for i=1:size(beta) 
+         output=output+beta(i)*x.^(i-1);
+    end
+ end
