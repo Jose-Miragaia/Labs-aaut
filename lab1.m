6c0e3b5be8f0c94daae377d9d0299b0537bb41c8
Jose-Miragaia <jpedroboavida@hotmail.com>
2017-10-12 00:25:04 +0100

    hello
    Hello my name is Miragaia how do u do?

--- lab1.m
+++ lab1.m
@@ -4,8 +4,11 @@
 %Pergunta 3;
 clear 
 close all
+
+
 load data1
 %a)e b) ,é pedido que a ordem 1 logo p =1
+
 p=1;
 [erro,Beta] = matrizfazer(p,x,y);
 
@@ -14,6 +17,8 @@ Beta
 
 %o valore do erro
 erro
+
+
 %%
 %pergunta 4
 %a)e b) ,é pedido que a ordem 1 logo p =2
@@ -46,24 +51,26 @@ erro
 %
 load data3
 % 
+
 for j=1:3
      med = sum(X(:,j))/50;
      for i=1:50
          X(i,j)= X(i,j) - med;
      end
 end
+
 [B,FitInfo] = lasso(X,Y);
 lassoPlot(B,FitInfo,'PlotType','Lambda','XScale','log');
 %%
-figure
+
 [B,FitInfo] = lasso(X,Y,'CV',50);
 lassoPlot(B,FitInfo,'PlotType','CV');
 FitInfo.Lambda1SE
 FitInfo.Index1SE
-beta_1 =B(1,FitInfo.Index1SE)
+beta_1 =B(1,FitInfo.Index1SE);
 %as seen in the graph b2 not cool man
-beta_2 =B(2,FitInfo.Index1SE)
-beta_3 =B(3,FitInfo.Index1SE)
+beta_2 =B(2,FitInfo.Index1SE);
+beta_3 =B(3,FitInfo.Index1SE);
 
 BETA = [beta_1;beta_2;beta_3]
 % for j=1:3
@@ -85,8 +92,10 @@ hello = sum(Y)/50;
 
 Y = Y - hello;
 
+title('Lasso com o labda escolhido');
 scatter(eixo,Y);
 scatter(eixo,Y_2,'r','X');
+legend('training_data','results with achieved beta');
 
 erro =0;
 for i=1:50
@@ -95,23 +104,82 @@ end
 % a maioria dos resultados, acho que todos não mudavam com tirar a medio dos X
 erro
 
-%% ridge
 
-Beta_ridge = inv(X'*X - eye(3)*FitInfo.Lambda1SE) *X'* Y;
-Y_R = X*Beta_ridge;
+%caso regular em que o lambda é 0
+
+beta_1 =B(1,1);
+%as seen in the graph b2 not cool man
+beta_2 =B(2,1);
+beta_3 =B(3,1);
+
+
+
+
+BETA = [beta_1;beta_2;beta_3]
+
+Y_LS=  X*BETA;
 
 figure
 hold on
+title('Lasso com o labda=0 (LS)');
+
 scatter(eixo,Y);
-scatter(eixo,Y_R,'r','X');
+scatter(eixo,Y_LS,'r','X');
+legend('training_data','results with LS method');
 
 erro =0;
 for i=1:50
-             erro = erro + (Y(i) - Y_R(i))^2;
+             erro = erro + (Y(i) - Y_LS(i))^2;
 end
-% a maioria dos resultados, acho que todos não mudavam com tirar a medio dos X
 erro
-%%
+
+% %% ridge
+% 
+Beta_ridge = inv(X'*X - eye(3)*FitInfo.Lambda1SE) *X'* Y
+
+Y_Lambda=  X*Beta_ridge;
+figure
+hold on
+title('Ridge com o labda escolhido no Lasso');
+
+scatter(eixo,Y);
+scatter(eixo,Y_Lambda,'r','X');
+legend('training_data','results with achieved beta');
+erro =0;
+for i=1:50
+             erro = erro + (Y(i) - Y_Lambda(i))^2;
+end
+erro
+
+BETA_R = ridge(Y,X,3)
+Y_Ridge=  X*BETA_R;
+figure
+hold on
+title('Ridge com o beta escolhido pelo função ridge()');
+scatter(eixo,Y);
+scatter(eixo,Y_Ridge,'r','X');
+legend('training_data','results with achieved beta through ridge');
+
+erro =0;
+for i=1:50
+             erro = erro + (Y(i) - Y_Ridge(i))^2;
+end
+erro
+
+% Y_R = X*Beta_ridge;
+% 
+% figure
+% hold on
+% scatter(eixo,Y);
+% scatter(eixo,Y_R,'r','X');
+% 
+% erro =0;
+% for i=1:50
+%              erro = erro + (Y(i) - Y_R(i))^2;
+% end
+% % a maioria dos resultados, acho que todos não mudavam com tirar a medio dos X
+% erro
+% %%
 % o erro neste caso vai ser mais elevado pois o lasso elimina totalmente o
 % b2 logo vai fazer com que o vai permanecer que o factor linear aumentando
 % o erro pois o lasso é mais severo com os betas menores do que o ridge
