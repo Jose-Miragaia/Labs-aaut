0cae2bdce8bb72345c1c889e89f49161c0565914
Jose-Miragaia <jpedroboavida@hotmail.com>
2017-11-12 11:33:26 +0000

    primeira pergunta onde a resposta não depende de nós

--- lab 4/lab_clear.m
+++ lab 4/lab_clear.m
@@ -21,21 +21,16 @@ for i=1:length(ytest)
     end
 end
 
-k=1;
-j=1;
-m=1;
-
-
 for i=1:length(ytrain)
     switch ytrain(i);
         case 1
-            X_1r(:,k) =  xtrain(:,i);
+            X_1(:,k) =  xtrain(:,i);
             k=k+1;
         case 2
-            X_2r(:,j) =  xtrain(:,i);
+            X_2(:,j) =  xtrain(:,i);
             j=j+1;
         case 3
-            X_3r (:,m)=  xtrain(:,i);
+            X_3 (:,m)=  xtrain(:,i);
             m=m+1;
     end
 end
@@ -44,23 +39,16 @@ end
 figure  
 hold on
 
+axis equal
+
 scatter (X_1(1,:),X_1(2,:))
 
 scatter (X_2(1,:),X_2(2,:),'X')
 
 scatter (X_3(1,:),X_3(2,:), 'd')
 
-scatter (X_1r(1,:),X_1r(2,:))
-
-scatter (X_2r(1,:),X_2r(2,:),'X')
-
-scatter (X_3r(1,:),X_3r(2,:), 'd')
 
 title('test data')
-axis equal
-
-legend('class1_{test}','class2_{test}','class3_{test}','class1_{train}','class2_{train}','class3_{train}')
-
-%%
 
 
+legend('class_1','class_2','class_3')
